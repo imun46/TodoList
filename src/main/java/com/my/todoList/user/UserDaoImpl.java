d4a52b2ebf1d92b9fc3a37f9dea4a71951472110
imun46 <do2816@gmail.com>
2024-11-10 05:29:47 +0900

    feat: userService 기능 구현

--- src/main/java/com/my/todoList/user/UserDaoImpl.java
+++ src/main/java/com/my/todoList/user/UserDaoImpl.java
@@ -1,26 +1,38 @@
 package com.my.todoList.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.my.todoList.user.mapper.UserMapper;
+
+@Repository
 public class UserDaoImpl implements UserDao{
+	
+	@Autowired
+	private UserMapper userMapper;
+	
+	public UserDaoImpl() {
+
+	}
 
 	@Override
 	public int insert(Users user) throws Exception {
-		
-		return 0;
+		return userMapper.insert(user);
 	}
 
 	@Override
 	public int update(Users user) throws Exception {
-		return 0;
+		return userMapper.update(user);
 	}
 
 	@Override
 	public Users select(Integer userNo) throws Exception {
-		return null;
+		return userMapper.select(userNo);
 	}
 
 	@Override
 	public int delete(Integer UserNo) throws Exception {
-		return 0;
+		return userMapper.delete(UserNo);
 	}
 
 }
---
+++ src/main/java/com/my/todoList/user/userService.java
@@ -0,0 +1,32 @@
+package com.my.todoList.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class userService {
+	@Autowired
+	private UserDao userDao;
+	
+	
+	/*회원가입*/
+	public Integer creatUser(Users user) throws Exception {
+		return userDao.insert(user);
+	}
+	
+	/*유저 정보 수정*/
+	public Integer updateUser(Users user) throws Exception {
+		return userDao.update(user);
+	}
+	
+	/*회원 탈퇴*/
+	public Integer deleteUser(Integer userNo) throws Exception {
+		return userDao.delete(userNo);
+	}
+	
+	/*특정회원 출력*/
+	public Users selecUserByNo(Integer userNo) throws Exception {
+		return userDao.select(userNo);
+	}
+	
+}
