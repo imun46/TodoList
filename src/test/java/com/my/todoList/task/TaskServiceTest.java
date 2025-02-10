8b3c563493e9b24a44a3270a8eb517c08eea221f
imun46 <do2816@gmail.com>
2025-02-11 00:45:17 +0900

    test: 전부 주석

--- src/test/java/com/my/todoList/task/TaskServiceTest.java
+++ src/test/java/com/my/todoList/task/TaskServiceTest.java
@@ -1,65 +1,65 @@
-package com.my.todoList.task;
-
-import java.time.LocalDate;
-import java.util.Date;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import com.my.todoList.task.dto.TaskDto;
-
-@SpringBootTest
-class TaskServiceTest {
-	@Autowired
-	private TaskService taskService;
-	
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
+//package com.my.todoList.task;
+//
+//import java.time.LocalDate;
+//import java.util.Date;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import com.my.todoList.task.dto.TaskDto;
+//
+//@SpringBootTest
+//class TaskServiceTest {
+//	@Autowired
+//	private TaskService taskService;
+//	
+//	@BeforeEach
+//	void setUp() throws Exception {
+//	}
+//
+////	@Test
+//	void testCreateTask() throws Exception {
+//		LocalDate date = LocalDate.now();
+//		TaskDto task = TaskDto.builder()
+//				.title("똥싸기")
+//				.content("아침에 똥싸는게 좋잖아")
+//				.startDate(date)
+//				.endDate(date)
+//				.categoryNo(1)
+//				.userNo(2)
+//				.build();
+//		System.out.println(taskService.createTask(task));
+//	}
+//
+////	@Test
+//	void testDeleteTask() throws Exception {
+//		System.out.println(taskService.deleteTask(10));
+//	} 
+//
+////	@Test
+//	void testUpdate() throws Exception {
+//		LocalDate date = LocalDate.now();
+//		TaskDto task = TaskDto.builder()
+//				.taskNo(3)
+//				.title("똥싸기")
+//				.content("아침에 똥싸는게 좋잖아")
+//				.startDate(date)
+//				.endDate(date)
+//				.state("active")
+//				.build();
+//		System.out.println(taskService.update(task));
+//	}
+//	
 //	@Test
-	void testCreateTask() throws Exception {
-		LocalDate date = LocalDate.now();
-		TaskDto task = TaskDto.builder()
-				.title("똥싸기")
-				.content("아침에 똥싸는게 좋잖아")
-				.startDate(date)
-				.endDate(date)
-				.categoryNo(1)
-				.userNo(2)
-				.build();
-		System.out.println(taskService.createTask(task));
-	}
-
-//	@Test
-	void testDeleteTask() throws Exception {
-		System.out.println(taskService.deleteTask(10));
-	} 
-
-//	@Test
-	void testUpdate() throws Exception {
-		LocalDate date = LocalDate.now();
-		TaskDto task = TaskDto.builder()
-				.taskNo(3)
-				.title("똥싸기")
-				.content("아침에 똥싸는게 좋잖아")
-				.startDate(date)
-				.endDate(date)
-				.state("active")
-				.build();
-		System.out.println(taskService.update(task));
-	}
-	
-	@Test
-	void testFindTaskByTaskNo() throws Exception {
-		System.out.println(taskService.findTaskByTaskNo(12));
-	}
-
-//	@Test
-	void testFindTaskByUserNo() throws Exception {
-		System.out.println(taskService.findTaskByUserNo(2));
-	}
-
-}
+//	void testFindTaskByTaskNo() throws Exception {
+//		System.out.println(taskService.findTaskByTaskNo(12));
+//	}
+//
+////	@Test
+//	void testFindTaskByUserNo() throws Exception {
+//		System.out.println(taskService.findTaskByUserNo(2));
+//	}
+//
+//}
--- src/test/java/com/my/todoList/user/userServiceTest.java
+++ src/test/java/com/my/todoList/user/userServiceTest.java
@@ -1,57 +1,57 @@
-package com.my.todoList.user;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import com.my.todoList.user.dto.UserDto;
-
-@SpringBootTest
-class userServiceTest {
-	
-	@Autowired
-	private UserService userService;
-	
-//	@Test
-	void testCreatUser() throws Exception {
-		UserDto user = UserDto.builder()
-				.id("hhh")
-				.password("aaa")
-				.email("emfif@naver.com")
-				.build();
-		System.out.println(userService.creatUser(user));
-	}
-
-//	@Test
-	void testUpdateUser() throws Exception {
-		UserDto user = UserDto.builder()
-				.userNo(2)
-				.id("changeId")
-				.password("aaa")
-				.email("emfif@naver.com")
-				.build();
-		System.out.println(userService.updateUser(user));
-	}
-
-//	@Test
-	void testDeleteUser() throws Exception {
-		System.out.println(userService.deleteUser(3));
-	}
-
-	
-//	@Test
-	void testFindUserByUserNo() throws Exception {
-		System.out.println(userService.findUserByUserNo(2));
-	}
-
-//	@Test
-//	void testLoginUser() {
-//		fail("Not yet implemented");
+//package com.my.todoList.user;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import com.my.todoList.user.dto.UserDto;
+//
+//@SpringBootTest
+//class userServiceTest {
+//	
+//	@Autowired
+//	private UserService userService;
+//	
+////	@Test
+//	void testCreatUser() throws Exception {
+//		UserDto user = UserDto.builder()
+//				.id("hhh")
+//				.password("aaa")
+//				.email("emfif@naver.com")
+//				.build();
+//		System.out.println(userService.creatUser(user));
 //	}
-	@Test
-	void testUserAll() throws Exception {
-		System.out.println(userService.findUserAll());
-	}
-}
+//
+////	@Test
+//	void testUpdateUser() throws Exception {
+//		UserDto user = UserDto.builder()
+//				.userNo(2)
+//				.id("changeId")
+//				.password("aaa")
+//				.email("emfif@naver.com")
+//				.build();
+//		System.out.println(userService.updateUser(user));
+//	}
+//
+////	@Test
+//	void testDeleteUser() throws Exception {
+//		System.out.println(userService.deleteUser(3));
+//	}
+//
+//	
+////	@Test
+//	void testFindUserByUserNo() throws Exception {
+//		System.out.println(userService.findUserByUserNo(2));
+//	}
+//
+////	@Test
+////	void testLoginUser() {
+////		fail("Not yet implemented");
+////	}
+//	@Test
+//	void testUserAll() throws Exception {
+//		System.out.println(userService.findUserAll());
+//	}
+//}
